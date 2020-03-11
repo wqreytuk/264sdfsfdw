55e9b845f1b8356fe4823ae871ab2ba1a437e3fd
123 <asd@asd.com>
2020-03-11 22:11:35 +0800

    asd

--- 246y5rw3hfsv - Copy - Copy - Copy - Copy - Copy/src/test/java/com/itheima/test/JdbcTemplateTest.java
+++ 246y5rw3hfsv - Copy - Copy - Copy - Copy - Copy/src/test/java/com/itheima/test/JdbcTemplateTest.java
@@ -3,6 +3,7 @@ package com.itheima.JdbcTemplate;
 import com.itheima.dao.IAccountDao;
 import com.itheima.domain.Account;
 import com.itheima.service.IAccountService;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,19 +13,14 @@ import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.List;
 
-/*
-账户的实体类
- */
-
-
-public class JdbcTemplateDemo1 {
+public class JdbcTemplateTest {
     public static void main(String[] args) throws SQLException {
         ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         IAccountDao accountDao = ac.getBean("accountDao", IAccountDao.class);
 
         List<Account> accounts = accountDao.findAllAccount();
-        for (Account account:accounts
-             ) {
+        for (Account account : accounts
+        ) {
             System.out.println(account.getName());
         }
     }
